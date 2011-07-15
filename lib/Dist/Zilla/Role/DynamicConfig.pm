2cca64675192f7329709723c03c78d6442003da6
Randy Stauner <rwstauner@cpan.org>
2011-07-15 00:52:20 +0000

    Import of RWSTAUNER/Dist-Zilla-Role-DynamicConfig-1.002 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Role-DynamicConfig
    gitpan-cpan-version:      1.002
    gitpan-cpan-path:         RWSTAUNER/Dist-Zilla-Role-DynamicConfig-1.002.tar.gz
    gitpan-cpan-author:       RWSTAUNER
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Role/DynamicConfig.pm
+++ lib/Dist/Zilla/Role/DynamicConfig.pm
@@ -1,14 +1,25 @@
+# vim: set ts=2 sts=2 sw=2 expandtab smarttab:
+#
+# This file is part of Dist-Zilla-Role-DynamicConfig
+#
+# This software is copyright (c) 2010 by Randy Stauner.
+#
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+#
+use strict;
+use warnings;
+
 package Dist::Zilla::Role::DynamicConfig;
 BEGIN {
-  $Dist::Zilla::Role::DynamicConfig::VERSION = '1.001002';
+  $Dist::Zilla::Role::DynamicConfig::VERSION = '1.002';
 }
 BEGIN {
   $Dist::Zilla::Role::DynamicConfig::AUTHORITY = 'cpan:RWSTAUNER';
 }
 # ABSTRACT: A Role that accepts a dynamic configuration
 
-use strict;
-use warnings;
+use Dist::Zilla 4 ();
 use Moose::Role;
 
 
@@ -19,24 +30,26 @@ has _config => (
 );
 
 
+# TODO: use around ? call $orig-> ? call super() ?
+
 sub BUILDARGS {
-	my ($class, @arg) = @_;
-	my %copy = ref $arg[0] ? %{$arg[0]} : @arg;
+  my ($class, @arg) = @_;
+  my %copy = ref $arg[0] ? %{$arg[0]} : @arg;
 
-	my $zilla = delete $copy{zilla};
-	my $name  = delete $copy{plugin_name};
+  my $zilla = delete $copy{zilla};
+  my $name  = delete $copy{plugin_name};
 
-	confess 'do not try to pass _config as a build arg!'
-		if $copy{_config};
+  confess 'do not try to pass _config as a build arg!'
+    if $copy{_config};
 
-	my $other = $class->separate_local_config(\%copy);
+  my $other = $class->separate_local_config(\%copy);
 
-	return {
-		zilla => $zilla,
-		plugin_name => $name,
-		_config     => \%copy,
-		%$other
-	}
+  return {
+    zilla => $zilla,
+    plugin_name => $name,
+    _config     => \%copy,
+    %$other
+  }
 }
 
 
@@ -49,7 +62,8 @@ no Moose::Role;
 __END__
 =pod
 
-=for :stopwords Randy Stauner BUILDARGS CPAN AnnoCPAN RT CPANTS Kwalitee diff
+=for :stopwords Randy Stauner BUILDARGS cpan testmatrix url annocpan anno bugtracker rt
+cpants kwalitee diff irc mailto metadata placeholders
 
 =head1 NAME
 
@@ -57,7 +71,7 @@ Dist::Zilla::Role::DynamicConfig - A Role that accepts a dynamic configuration
 
 =head1 VERSION
 
-version 1.001002
+version 1.002
 
 =head1 DESCRIPTION
 
@@ -82,7 +96,7 @@ Do not attempt to assign to this from your F<dist.ini>.
 Copied/modified from L<Dist::Zilla::Plugin::Prereqs>
 to allow arbitrary values to be specified.
 
-This overwrites the L<Class::MOP::Instance> method
+This overwrites the L<Moose::Object> method
 called to prepare arguments before instantiation.
 
 It separates the expected arguments
@@ -102,11 +116,16 @@ Required.
 
 =head1 SUPPORT
 
+=head2 Perldoc
+
 You can find documentation for this module with the perldoc command.
 
   perldoc Dist::Zilla::Role::DynamicConfig
 
 =head2 Websites
+
+The following websites have more information about this module, and may be of help to you. As always,
+in addition to those websites please use your favorite search engine to discover more resources.
 
 =over 4
 
@@ -114,64 +133,64 @@ You can find documentation for this module with the perldoc command.
 
 Search CPAN
 
+The default CPAN search engine, useful to view POD in HTML format.
+
 L<http://search.cpan.org/dist/Dist-Zilla-Role-DynamicConfig>
 
 =item *
 
 RT: CPAN's Bug Tracker
 
+The RT ( Request Tracker ) website is the default bug/issue tracking system for CPAN.
+
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dist-Zilla-Role-DynamicConfig>
-
-=item *
-
-AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Dist-Zilla-Role-DynamicConfig>
 
 =item *
 
 CPAN Ratings
 
+The CPAN Ratings is a website that allows community ratings and reviews of Perl modules.
+
 L<http://cpanratings.perl.org/d/Dist-Zilla-Role-DynamicConfig>
 
 =item *
 
-CPAN Forum
+CPAN Testers
 
-L<http://cpanforum.com/dist/Dist-Zilla-Role-DynamicConfig>
+The CPAN Testers is a network of smokers who run automated tests on uploaded CPAN distributions.
 
-=item *
-
-CPANTS Kwalitee
-
-L<http://cpants.perl.org/dist/overview/Dist-Zilla-Role-DynamicConfig>
-
-=item *
-
-CPAN Testers Results
-
-L<http://cpantesters.org/distro/D/Dist-Zilla-Role-DynamicConfig.html>
+L<http://www.cpantesters.org/distro/D/Dist-Zilla-Role-DynamicConfig>
 
 =item *
 
 CPAN Testers Matrix
 
+The CPAN Testers Matrix is a website that provides a visual overview of the test results for a distribution on various Perls/platforms.
+
 L<http://matrix.cpantesters.org/?dist=Dist-Zilla-Role-DynamicConfig>
+
+=item *
+
+CPAN Testers Dependencies
+
+The CPAN Testers Dependencies is a website that shows a chart of the test results of all dependencies for a distribution.
+
+L<http://deps.cpantesters.org/?module=Dist::Zilla::Role::DynamicConfig>
 
 =back
 
-=head2 Bugs
+=head2 Bugs / Feature Requests
 
-Please report any bugs or feature requests to C<bug-dist-zilla-role-dynamicconfig at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dist-Zilla-Role-DynamicConfig>.  I will be
-notified, and then you'll automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests by email to C<bug-dist-zilla-role-dynamicconfig at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dist-Zilla-Role-DynamicConfig>. You will be automatically notified of any
+progress on the request by the system.
 
 =head2 Source Code
 
 
-L<http://github.com/magnificent-tears/Dist-Zilla-Role-DynamicConfig/tree>
+L<http://github.com/rwstauner/Dist-Zilla-Role-DynamicConfig>
 
-  git clone git://github.com/magnificent-tears/Dist-Zilla-Role-DynamicConfig.git
+  git clone http://github.com/rwstauner/Dist-Zilla-Role-DynamicConfig
 
 =head1 AUTHOR
 
--- t/lib/Dist/Zilla/Plugin/TestDynamicConfig.pm
+++ t/lib/Dist/Zilla/Plugin/TestDynamicConfig.pm
@@ -1,3 +1,12 @@
+# vim: set ts=2 sts=2 sw=2 expandtab smarttab:
+#
+# This file is part of Dist-Zilla-Role-DynamicConfig
+#
+# This software is copyright (c) 2010 by Randy Stauner.
+#
+# This is free software; you can redistribute it and/or modify it under
+# the same terms as the Perl 5 programming language system itself.
+#
 package Dist::Zilla::Plugin::TestDynamicConfig;
 # ABSTRACT: Test Dist::Zilla::Role::DynamicConfig
 
@@ -5,20 +14,20 @@ use strict;
 use warnings;
 use Moose;
 with qw(
-	Dist::Zilla::Role::DynamicConfig
-	Dist::Zilla::Role::Plugin
+  Dist::Zilla::Role::DynamicConfig
+  Dist::Zilla::Role::Plugin
 );
 
 has 'extra' => (
-	is      => 'ro',
-	isa     => 'Str',
-	default => '',
+  is      => 'ro',
+  isa     => 'Str',
+  default => '',
 );
 
 sub separate_local_config {
-	my ($self, $config) = @_;
-	my %other = (extra => delete $config->{extra} || '');
-	return \%other;
+  my ($self, $config) = @_;
+  my %other = (extra => delete $config->{extra} || '');
+  return \%other;
 }
 
 no Moose;
